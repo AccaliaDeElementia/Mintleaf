c719f2d6ab2d8df6e1761aa1a535a0f8a76d66b2
Accalia de Elementia <Accalia.de.Elementia@gmail.com>
2013-12-13 20:34:47 -0500

    Final(?) version of node js version of this. Maybe I'll reimplement in python?

--- Mintleaf.js
+++ Mintleaf.js
@@ -1,4 +1,24 @@
-/*jslint node: true, maxlen: 80 */
+/*jslint node: true, maxlen: 80, unparam: true */
 'use strict';
-var basename = new RegExp('[^/]+$'),
-    base = process.mainModule.filename.replace(basename, '');
+var http = require('http'),
+    io = require('socket.io'),
+    Mintleaf = require('./lib/mintleaf'),
+    mintleaf = new Mintleaf('/storage/accalia/images'),
+    app,
+    socketer;
+
+function handler(req, res) {
+    res.writeHead(500);
+    res.end('Not Implemented. Access static files through nginx');
+}
+app = http.createServer(handler);
+app.listen(50998);
+socketer = io.listen(app, {
+    'log level': 1
+});
+
+socketer.sockets.on('connection', function (socket) {
+    socket.on('listdir', mintleaf.listdir);
+    socket.on('getfile', mintleaf.getfile);
+});
+
